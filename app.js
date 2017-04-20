79c0edb2620fe2ae97a6119503323164a68247e9
dkobierski <dawid.kobierski@gmail.com>
2017-04-20 21:09:50 +0200

    Add AVS support

--- app.js
+++ app.js
@@ -1,22 +1,67 @@
+const https = require('https')
+const fs = require('fs')
+const request = require('request')
+
+const options = {
+  // Private Key
+  key: fs.readFileSync('./ssl/server.key'),
+
+  // SSL Certficate
+  cert: fs.readFileSync('./ssl/server.crt'),
+
+  // Make sure an error is not emitted on connection when the server certificate verification against the list of supplied CAs fails.
+  rejectUnauthorized: false
+}
+
 const express = require('express')
 const morgan = require('morgan')
 const path = require('path')
-
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const qs = require('qs')
 const app = express()
+const port = 3000
 
+const server = https.createServer(options, app).listen(port, () => {
+  console.log(`BookingBot.Web running at https://localhost:${port}`)
+})
+
+app.use(bodyParser.urlencoded({ extended: true}))
+app.use(bodyParser.json());
+app.use(cors());
 app.use(morgan('combined'))
+
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'))
 })
 
-app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'public/404.html'))
+app.get('/authresponse', (req, res) => {
+  res.redirect(301, `/?${qs.stringify(req.query)}`);
 })
 
-const PORT = 3000
+app.get('/parse-m3u', (req, res) => {
+  const m3uUrl = req.query.url
+  console.log(m3uUrl)
 
-app.listen(PORT, () =>{
-  console.log(`BookingBot.Web running at htttp://localhost:${PORT}`)
+  if (!m3uUrl) {
+    return res.json([])
+  }
+
+  const urls = []
+
+  request(m3uUrl, function(error, response, bodyResponse) {
+    console.log(bodyResponse, m3uUrl)
+    if (bodyResponse) {
+      urls.push(bodyResponse)
+    }
+
+    res.json(urls)
+  })
+})
+
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'public/404.html'))
 })
--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,160 @@
+const AVS = require('alexa-voice-service')
 
+const avs = new AVS({
+  debug: true,
+  clientId: 'amzn1.application-oa2-client.64708316a35a41d3ac744b7dfd1d8e9e',
+  deviceId: 'projektryanair',
+  deviceSerialNumber: 123,
+  redirectUri: `https://${window.location.host}/authresponse`
+})
+window.avs = avs
+
+avs.on(AVS.EventTypes.TOKEN_INVALID, () => {
+  avs.logout()
+})
+
+avs.on(AVS.EventTypes.LOGIN, () => {
+  record.disabled = false
+})
+
+avs.on(AVS.EventTypes.LOGOUT, () => {
+  record.disabled = true
+})
+
+const loginBtn = document.getElementById('plane')
+const record = document.getElementById('start-booking')
+
+avs.getTokenFromUrl()
+.then(() => avs.getToken())
+.then(token => localStorage.setItem('token', token))
+.then(() => avs.requestMic())
+.catch(() => {
+  const cachedToken = localStorage.getItem('token')
+
+  if (cachedToken) {
+    avs.setToken(cachedToken)
+    return avs.requestMic()
+  }
+})
+
+loginBtn.addEventListener('click', login)
+
+function login() {
+  return avs.login()
+  .then(() => avs.requestMic())
+  .catch(() => {})
+}
+
+record.addEventListener('mousedown', () => {
+  avs.startRecording()
+})
+
+record.addEventListener('mouseup', () => {
+  avs.stopRecording().then(dataView => {
+    avs.player.emptyQueue()
+    .then(() => avs.audioToBlob(dataView))
+    .catch(error => {
+      console.error(error)
+    })
+
+    avs.sendAudio(dataView)
+    .then(({xhr, response}) => {
+
+      var promises = []
+      var audioMap = {}
+      var directives = null
+
+      if (response.multipart.length) {
+        response.multipart.forEach(multipart => {
+          let body = multipart.body
+          if (multipart.headers && multipart.headers['Content-Type'] === 'application/json') {
+            try {
+              body = JSON.parse(body)
+            } catch(error) {
+              console.error(error)
+            }
+
+            if (body && body.messageBody && body.messageBody.directives) {
+              directives = body.messageBody.directives
+            }
+          } else if (multipart.headers['Content-Type'] === 'audio/mpeg') {
+            const start = multipart.meta.body.byteOffset.start
+            const end = multipart.meta.body.byteOffset.end
+
+            /**
+             * Not sure if bug in buffer module or in http message parser
+             * because it's joining arraybuffers so I have to this to
+             * seperate them out.
+             */
+            var slicedBody = xhr.response.slice(start, end)
+
+            //promises.push(avs.player.enqueue(slicedBody))
+            audioMap[multipart.headers['Content-ID']] = slicedBody
+          }
+        })
+
+        function findAudioFromContentId(contentId) {
+          contentId = contentId.replace('cid:', '')
+          for (var key in audioMap) {
+            if (key.indexOf(contentId) > -1) {
+              return audioMap[key]
+            }
+          }
+        }
+
+        directives.forEach(directive => {
+          if (directive.namespace === 'SpeechSynthesizer') {
+            if (directive.name === 'speak') {
+              const contentId = directive.payload.audioContent
+              const audio = findAudioFromContentId(contentId)
+              if (audio) {
+                promises.push(avs.player.enqueue(audio))
+              }
+            }
+          } else if (directive.namespace === 'AudioPlayer') {
+            if (directive.name === 'play') {
+              const streams = directive.payload.audioItem.streams
+              streams.forEach(stream => {
+                const streamUrl = stream.streamUrl
+
+                const audio = findAudioFromContentId(streamUrl)
+                if (audio) {
+                  promises.push(avs.player.enqueue(audio))
+                } else if (streamUrl.indexOf('http') > -1) {
+                  const xhr = new XMLHttpRequest()
+                  const url = `/parse-m3u?url=${streamUrl.replace(/!.*$/, '')}`
+                  xhr.open('GET', url, true)
+                  xhr.responseType = 'json'
+                  xhr.onload = (event) => {
+                    const urls = event.currentTarget.response
+
+                    urls.forEach(url => {
+                      avs.player.enqueue(url)
+                    })
+                  }
+                  xhr.send()
+                }
+              })
+            } else if (directive.namespace === 'SpeechRecognizer') {
+              if (directive.name === 'listen') {
+                const timeout = directive.payload.timeoutIntervalInMillis
+                // enable mic
+              }
+            }
+          }
+        })
+
+        if (promises.length) {
+          Promise.all(promises)
+         .then(() => {
+            avs.player.playQueue()
+          })
+        }
+      }
+
+    })
+    .catch(error => {
+      console.error(error)
+    })
+  })
+})
